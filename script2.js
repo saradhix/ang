292a10a0259671b7b2fdf1884ab2f36597174411
Saradhi <saradhix@yahoo.com>
2016-10-27 21:59:29 +0530

    Using ng-repeat

--- script2.js
+++ script2.js
@@ -3,6 +3,12 @@ var myApp = angular.module("myModule",[]);
 var myController = function($scope)
 {
     $scope.message = "My controller";
+    var employees=[
+    { name:"Vijay", age:20},
+    { name:"SUbbu", age:30},
+    { name:"Sandy", age:40}
+    ];
+    $scope.employees=employees;
     
 }
 
